d15e185b9b85a92a14e58cce7a43acbe3e3f2fb8
ChrisLu <chrislu@github.com>
2019-01-10 18:03:33 +0800

    add files

---
+++ components/BtnError/BtnError.vue
@@ -0,0 +1,24 @@
+<template>
+  <button v-if="visible" @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    clicked: false,
+    visible: false
+  }),
+  methods: {
+    handleClick() {
+      this.clicked = true;
+    }
+  }
+};
+</script>
